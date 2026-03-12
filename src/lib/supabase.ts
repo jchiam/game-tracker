0237aa0293df72f9c20522c2d545978aaf7a97a2
Jonathan Chiam <jczjchiam@gmail.com>
2026-03-12 11:49:28 +0800

    Revert supabase env var keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.STORAGE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.STORAGE_SUPABASE_ANON_KEY || '';
+console.log(import.meta.env);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+console.log('test');
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
