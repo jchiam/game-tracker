4d57d51b12e34cc54b8271136b2033cc4d339f3c
Jonathan Chiam <jczjchiam@gmail.com>
2026-03-12 11:51:48 +0800

    Remove console.log

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log(import.meta.env);
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-console.log('test');
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
 
 // Create a single supabase client for interacting with your database
