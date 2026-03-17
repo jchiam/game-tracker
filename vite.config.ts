8a8de2fd91699e38272f1916fe34fd4dcc5d3b3f
Jonathan Chiam <jczjchiam@gmail.com>
2026-03-17 11:55:16 +0800

    Fix test setup
    Fix playwright tests

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+  },
   test: {
     globals: true,
     exclude: ['**/node_modules/**', '**/dist/**', '**/tests/**'],
